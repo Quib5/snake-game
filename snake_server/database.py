f7c94f68868e5f86d9d4c31a44129b5954f14f4f
Quib5 <167481775+Quib5@users.noreply.github.com>
2025-12-06 11:00:13 -0700

    Add files via upload

---
+++ snake_server/database.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("snake.db")
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password BLOB
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS scores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            score INTEGER
+        );
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user(username, password_hash):
+    try:
+        conn = sqlite3.connect("snake.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password_hash))
+        conn.commit()
+        conn.close()
+        return True
+    except:
+        return False
+
+
+def verify_user(username):
+    conn = sqlite3.connect("snake.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+    user = cur.fetchone()
+    conn.close()
+    return user
+
+
+def add_score(username, score):
+    conn = sqlite3.connect("snake.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO scores (username, score) VALUES (?, ?)", (username, score))
+    conn.commit()
+    conn.close()
+
+
+def get_leaderboard():
+    conn = sqlite3.connect("snake.db")
+    cur = conn.cursor()
+    cur.execute("SELECT username, score FROM scores ORDER BY score DESC LIMIT 20")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
